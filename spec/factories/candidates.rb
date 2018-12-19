b093e763fbf38cb81bec4a7d270e0b57c16fb6b5
Raquel Queiroz <eu.raquell@hotmail.com>
2018-12-19 00:23:33 -0200

    tests to candidates request

---
+++ spec/factories/candidates.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :candidate do
+    name { "Ariel Ha" }
+    profession { "QA" }
+    location { "A" }
+    level { 2 }
+  end
+end
---
+++ spec/requests/api/v1/candidates_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'Candidates API', type: :request do
+  describe 'POST /api/v1/pessoas' do
+    let(:candidates_url) { '/api/v1/pessoas' }
+    let(:candidate_attributes) do
+      {
+        nome: "John Doe",
+        profissao: "Engenheiro de Software",
+        localizacao: "C",
+        nivel: 2
+      }
+    end
+
+    context 'when the request is valid' do
+      before { post candidates_url, params: candidate_attributes }
+
+      it 'creates a candidate' do
+        expect(json['name']).to eq("John Doe")
+      end
+
+      it 'returns the status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post candidates_url, params: { nome: 'Foobar' } }
+
+      it 'returns the status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match("{\"profession\":[\"can't be blank\"],\"location\":[\"can't be blank\"],\"level\":[\"can't be blank\"]}")
+      end
+    end
+  end
+end
